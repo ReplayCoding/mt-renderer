a94aa2296b5559fb5c4a796c9eec21c43c028bc4
ReplayCoding <replaycoding@gmail.com>
2024-02-13 18:38:03 -0800

    init project

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      # For `nix develop`:
+      devShell = forAllSystems (system:
+        let pkgs = (import nixpkgs) { inherit system; };
+        in pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo rustfmt clippy ];
+        });
+    };
+}
+
